eb073eb8feffd7a157b7007719a37fd46b656171
mohitsahu21 <mohitsahu1993@gmail.com>
2023-07-23 00:32:50 +0530

    layout part done

---
+++ front-end-coding-challange/src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import './App.css';
+import Repository from "./Repository";
+
+function App() {
+  
+  return (
+    <div className="container">
+      <h1 className="main-heading">Most Starred Repos</h1>
+      <Repository/>
+      <Repository/>
+      <Repository/>
+      <Repository/>
+      <Repository/>
+      <Repository/>
+      <Repository/>
+      <Repository/>
+      
+  </div>
+  );
+}
+
+export default App;
